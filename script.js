f3f741da42111e76b89fcd0730d1a7f24a7096ea
Vipin Yadav <imvpn22@gmail.com>
2020-04-04 13:01:23 +0530

    added mode switched

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hour = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const sec = document.querySelector('.sec');
 
-setInterval(() => {
+setClock = () => {
   let day = new Date();
   let hh = day.getHours() * 30;
   let mm = day.getMinutes() * deg;
@@ -12,4 +12,27 @@ setInterval(() => {
   hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
   min.style.transform = `rotateZ(${mm}deg)`;
   sec.style.transform = `rotateZ(${ss}deg)`;
-}, 1000);
+};
+
+setInterval(setClock, 1000);
+
+setLightMode = () => {
+  document.documentElement.style.setProperty('--main-bg-color', '#fff');
+  document.documentElement.style.setProperty('--main-text-color', '#888');
+};
+
+setDarkMode = () => {
+  document.documentElement.style.setProperty('--main-bg-color', '#091921');
+  document.documentElement.style.setProperty('--main-text-color', '#ccc');
+};
+
+const switchModeBtn = document.querySelector('.switch-btn');
+switchModeBtn.addEventListener('click', () => {
+  if (switchModeBtn.textContent.toLowerCase() === 'dark') {
+    switchModeBtn.textContent = 'light';
+    setDarkMode();
+  } else {
+    switchModeBtn.textContent = 'dark';
+    setLightMode();
+  }
+});
